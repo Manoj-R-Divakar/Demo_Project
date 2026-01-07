0031264d7f5145ec15422150271be2b78551a245
Pavan_H <officialpavanh@gmail.com>
2026-01-07 21:06:48 +0530

    Added pavanController

---
+++ src/main/java/com/example/demo_java/pavanContrller/Pavan.java
@@ -0,0 +1,11 @@
+package com.example.demo_java.pavanContrller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Pavan {
+
+    public String hellopavan(){
+        return "Hi I am Pavan";
+    }
+}
